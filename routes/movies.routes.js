4e5af596614bf1be531a0e56a1575eac684a5391
Gerard Solanes <gerardsolher@gmail.com>
2023-01-16 19:37:42 +0100

    Adds Movie model

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
+const Celebrity = require('../models/Celebrity.model');
 
+//GET all movies
+// ROUTE: /movies
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('movies/movies');
 });
 
 module.exports = router;
