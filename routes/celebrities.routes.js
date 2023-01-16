8da95a199cf97aac0600ded812bef61fdf90af0c
Gerard Solanes <gerardsolher@gmail.com>
2023-01-16 19:19:08 +0100

    Implements post method for new celebrity

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Celebrity = require('../models/Celebrity.model');
 
+// GET all celebrities
+// ROUTE: /celebrities
 router.get('/', (req, res, next) => {
     res.render('celebrities/celebrities');
 });
 
+// GET form for new celebrity
+// ROUTE: /celebrities/create
 router.get('/create', (req, res, next) => {
     res.render('celebrities/new-celebrity');
-})
+});
 
-router.post('/create', (req, res, next) => {
-    
-})
+// POST for new celebrity
+// ROUTE: /celebrities/create
+router.post('/create', async (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    try {
+        await Celebrity.create({ name, occupation, catchPhrase });
+        res.redirect('/celebrities');
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
